1aadd5cf05bf33851ec96e645b16df8f73e498dc
BhabhaSeptember <septemberbhabha@gmail.com>
2024-05-17 13:37:57 +0200

    edit

--- Lesson2/messages.js
+++ Lesson2/messages.js
@@ -3,5 +3,5 @@
 let messages = [
   "A change of environment can be a good thing!",
   "You will make it!",
-  "Just run with the code!"
+  "Just run with the code!",
 ];
--- Lesson2/positiveMessages.js
+++ Lesson2/positiveMessages.js
@@ -3,7 +3,7 @@
 let messages = [
   "A change of environment can be a good thing!",
     "You will make it!",
-      "Just run with the code!"
+      "Just run with the code!",
       ];
       
 messages.forEach(message => console.log(message));
