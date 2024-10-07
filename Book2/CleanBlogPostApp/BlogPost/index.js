930cbae926174a71cc8650ed27b0517bf7da911e
BhabhaSeptember <septemberbhabha@gmail.com>
2024-10-07 10:50:45 +0200

    commit

--- Book2/CleanBlogPostApp/BlogPost/index.js
+++ Book2/CleanBlogPostApp/BlogPost/index.js
@@ -5,6 +5,15 @@ const mongoose = require("mongoose");
 const BlogPost = require('./models/BlogPost.js');
 const fileUpload = require('express-fileupload') 
 
+const newPostController = require('./controllers/newPost');
+const getPostController = require('./controllers/getPost');
+const storePostController = require('./controllers/storePost');
+const pagesController = require('./controllers/pagesController');
+const homeController = require("./controllers/home");
+
+const validateMiddleWare = require("./middleware/validationMiddleware"); 
+
+
 const app = new express();
 
 mongoose.connect("mongodb://localhost/blog_db", { useNewUrlParser: true });
@@ -12,19 +21,19 @@ mongoose.connect("mongodb://localhost/blog_db", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 
 //CUSTOM MIDDLEWARE
-const customMiddleWare = (req,res,next)=>{ 
-  console.log('Custom middle ware called'); 
-  next(); 
-  }; 
+// const customMiddleWare = (req,res,next)=>{ 
+//   console.log('Custom middle ware called'); 
+//   next(); 
+//   }; 
 
-//VALID MIDDLEWARE
-  const validateMiddleWare = (req,res,next)=>{     
-    if(req.files === null || req.body.title === null){  
-    console.log("Invalid Post...");         
-    return res.redirect('/posts/new') ;
-    }     
-    next() ;
-  };
+//VALIDATION MIDDLEWARE
+  // const validateMiddleWare = (req,res,next)=>{     
+  //   if(req.files === null || req.body.title === null){  
+  //   console.log("Invalid Post...");         
+  //   return res.redirect('/posts/new') ;
+  //   }     
+  //   next() ;
+  // };
 
   
 
@@ -32,7 +41,7 @@ app.use(express.static("public"));
 app.use(express.json()); 
 app.use(express.urlencoded());
 app.use(fileUpload()); 
-app.use(customMiddleWare); 
+// app.use(customMiddleWare); 
 app.use('/posts/store', validateMiddleWare); 
 
 
@@ -54,30 +63,16 @@ app.use('/posts/store', validateMiddleWare);
 // });
 
 //ROUTES WITH EJS
-app.get("/", async (req, res) => {
-  const blogposts = await BlogPost.find({});
-  // console.log(blogposts);
-  res.render("index", {blogposts});
-});
+app.get("/", homeController);
 
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-app.get("/contact", (req, res) => {
-  res.render("contact");
-});
+app.get("/about", pagesController.about);
+app.get("/contact", pagesController.contact);
 
 
 //VIEW SINGLE POST BY ID
-app.get("/post/:id", async (req, res) => {
-  const blogpost = await BlogPost.findById(req.params.id)
-  res.render("post", {blogpost});
-});
+app.get("/post/:id", getPostController);
 
-app.get("/posts/new", (req, res) => {
-  res.render("create");
-});
+app.get("/posts/new", newPostController);
 
 //STORE POST IN DATABASE 
 // app.post("/posts/store", (req, res) => {
@@ -109,34 +104,7 @@ app.get("/posts/new", (req, res) => {
 // })             
 // }) 
 
-app.post('/posts/store', (req, res) => {
-  console.log(req.files); // Log req.files to see its content
-
-  if (!req.files || !req.files.image) {
-    return res.status(400).send("No image file uploaded.");
-  }
-
-  let image = req.files.image;
-  const imagePath = path.resolve(__dirname, 'public/assets', image.name);
-
-  image.mv(imagePath, async (error) => {
-    if (error) {
-      console.error("File upload error:", error);
-      return res.status(500).send("Error while uploading image.");
-    }
-
-    try {
-      await BlogPost.create({
-        ...req.body,
-        image: '/assets/' + image.name // Make sure this path matches where you store the image
-      });
-      res.redirect('/');
-    } catch (dbError) {
-      console.error("Database error:", dbError);
-      return res.status(500).send("Error while saving post to database.");
-    }
-  });
-});
+app.post('/posts/store', storePostController);
 
 app.listen(4000, () => {
   console.log("App listening on port 4000");
